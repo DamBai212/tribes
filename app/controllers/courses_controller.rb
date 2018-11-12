d71ed2f00b920856e214f8c915a518efc2b22ce4
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-11-12 16:45:35 +0000

    Controllers added for users courses lectures

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,47 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @courses = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.create(course_params)
+    redirect_to @course
+  end
+
+  def show
+
+  end
+
+  def edit
+    
+  end
+
+  def update
+    @course.update(course_params)
+    redirect_to @course
+  end
+
+  def destroy
+    @course.destroy
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(
+      :title,
+      :category,
+      :created_on
+    )
+  end
 end
--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,2 +1,49 @@
 class LecturesController < ApplicationController
+  before_action :set_lecture, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @lectures = Lecture.all
+  end
+
+  def new
+    @lecture = Lecture.new
+  end
+
+  def create
+    @lecture = Lecture.create(lecture_params)
+    redirect_to @lecture
+  end
+
+  def show
+
+  end
+
+  def edit
+    
+  end
+
+  def update
+    @lecture.update(lecture_params)
+    redirect_to @lecture
+  end
+
+  def destroy
+    @lecture.destroy
+  end
+
+  private
+
+  def set_lecture
+    @lecture = Lecture.find(params[:id])
+  end
+
+  def lecture_params
+    params.require(:lecture).permit(
+      :title,
+      :category,
+      :summary,
+      :position,
+      :course_id
+    )
+  end
 end
